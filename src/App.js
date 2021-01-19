3e0832e3fa2313d3037014cda6e3d701b654fafe
sinha97 <vk9633698@gmail.com>
2021-01-19 17:11:10 +0530

    Login form added

--- src/App.js
+++ src/App.js
@@ -2,17 +2,22 @@ import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 import Checkout from "./components/Checkout/Checkout";
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Login from './components/Login/login';
   
 function App() {
   return (
     <Router>
       <div className="App">
-        <Header />
         <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
           <Route path="/checkout">
+            <Header />
             <Checkout />
           </Route>
           <Route path="/">
+            <Header />
             <Home />
           </Route>
         </Switch>
